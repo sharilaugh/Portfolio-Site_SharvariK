f53c31191958b39951de6e3e04c1da82e6715daf
sharilaugh <shari_laugh@yahoo.com>
2021-06-21 22:47:33 -0500

    JQuery added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hello world! Welcome to my portfolio site. Please surf around.");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
