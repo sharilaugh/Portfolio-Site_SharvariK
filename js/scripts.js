f07ad2a056d0b20d6337587f5864a0a3696b93c4
sharilaugh <shari_laugh@yahoo.com>
2021-07-05 13:51:34 -0500

    Fixed errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Hello world! Welcome to my portfolio site. Please surf around.");
+console.log('Hello world! Welcome to my portfolio site. Please surf around.')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
